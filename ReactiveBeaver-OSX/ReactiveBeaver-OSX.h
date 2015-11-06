2fc776fe9fb1399af399ec91669368d7ca90a9f3
skyylex <yury.lapitsky@gmail.com>
2015-11-06 18:38:49 +0300

    Added OSX target

---
+++ ReactiveBeaver-OSX/ReactiveBeaver-OSX.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveBeaver-OSX.h
+//  ReactiveBeaver-OSX
+//
+//  Created by Yury Lapitsky on 11/6/15.
+//  Copyright © 2015 skyylex. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ReactiveBeaver-OSX.
+FOUNDATION_EXPORT double ReactiveBeaver_OSXVersionNumber;
+
+//! Project version string for ReactiveBeaver-OSX.
+FOUNDATION_EXPORT const unsigned char ReactiveBeaver_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveBeaver_OSX/PublicHeader.h>
+
+
